a47e3155276398631f7b4c21cfe1e1139cea5a66
김젼이 <wlgus0321@gmail.com>
2022-09-02 15:26:19 +0900

    템플릿 수정

--- client/src/pages/Cart/Cart1.vue
+++ client/src/pages/Cart/Cart1.vue
@@ -1,147 +1,149 @@
 <template>
-<Header></Header>
-<div style="width: 100%; display: flex;">
+  <Header></Header>
+  <div style="width: 100%; display: flex;">
 
-    <div class="cartGoods">
-    <div  style=" margin:0 auto;">
+      <div class="cartGoods">
+      <div  style=" margin:0 auto;">
 
-      <q-markup-table :separator="separator" flat >
-        <thead>
-          <tr>
-            <th class="text-middle" style="font-weight: bold; font-size: 15px; background-color: #FAEBD7"> Item </th>
-            <th class="text-middle " style="font-weight: bold; font-size: 15px; background-color:#FAEBD7"> Detail</th>
-            <th class="text-middle" style="font-weight: bold; font-size: 15px; background-color:#FAEBD7"> Price</th>
-            <th class="text-middle" style="font-weight: bold; font-size: 15px; background-color:#FAEBD7"> Action</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <th class="text-middle">사진</th>
-            <th class="text-middle">아이폰</th>
-            <th class="text-middle">15,300</th>
-            <th class="text-middle"> <span class="removeContainer" style="color: red;" v-on:click="RemoveComment">
-        <i class="fa-solid fa-trash-can" aria-hidden="true"></i> </span> </th>
-          </tr>
-
-          <tr>
-            <th class="text-middle">사진2</th>
-            <th class="text-middle">아이폰2</th>
-            <th class="text-middle">15,300</th>
-            <th class="text-middle"> <span class="removeContainer" style="color: red;" v-on:click="RemoveComment">
-        <i class="fa-solid fa-trash-can" aria-hidden="true"></i> </span> </th>
-          </tr>
-
-          <tr>
-            <th class="text-middle">사진3</th>
-            <th class="text-middle">아이폰3</th>
-            <th class="text-middle">15,300</th>
-            <th class="text-middle"> <span class="removeContainer" style="color: red;" v-on:click="RemoveComment">
-        <i class="fa-solid fa-trash-can" aria-hidden="true"></i> </span> </th>
-          </tr>
-        </tbody>
-      </q-markup-table>
-    </div>
-    </div>
-
-    <div class="cartTotal">
-      <span
-      style="font-size: 23px;
-      font-family: 'Rubik', sans-serif;
-      margin-left: 20px;">Total</span>
-      <hr width="90%">
-
-      <div>
-        <div  style=" margin:0 auto;">
         <q-markup-table :separator="separator" flat >
-       <tbody>
-          <tr>
-            <th class="text-middle">아이폰</th>
-            <th class="text-middle">1개</th>
-            <th class="text-middle">15,300</th>
-          </tr>
+          <thead>
+            <tr>
+              <th class="text-middle" style="font-weight: bold; font-size: 15px; background-color: #FAEBD7"> Item </th>
+              <th class="text-middle " style="font-weight: bold; font-size: 15px; background-color:#FAEBD7"> Detail</th>
+              <th class="text-middle" style="font-weight: bold; font-size: 15px; background-color:#FAEBD7"> Price</th>
+              <th class="text-middle" style="font-weight: bold; font-size: 15px; background-color:#FAEBD7"> Action</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <th class="text-middle">사진</th>
+              <th class="text-middle">아이폰</th>
+              <th class="text-middle">15,300</th>
+              <th class="text-middle"> <span class="removeContainer" style="color: red;" v-on:click="RemoveComment">
+          <i class="fa-solid fa-trash-can" aria-hidden="true"></i> </span> </th>
+            </tr>
 
-          <tr>
-            <th class="text-middle">아이폰2</th>
-            <th class="text-middle">1개</th>
-            <th class="text-middle">15,300</th>
-          </tr>
+            <tr>
+              <th class="text-middle">사진2</th>
+              <th class="text-middle">아이폰2</th>
+              <th class="text-middle">15,300</th>
+              <th class="text-middle"> <span class="removeContainer" style="color: red;" v-on:click="RemoveComment">
+          <i class="fa-solid fa-trash-can" aria-hidden="true"></i> </span> </th>
+            </tr>
 
-          <tr>
-            <th class="text-middle">아이폰3</th>
-            <th class="text-middle">1개</th>
-            <th class="text-middle">15,300</th>
-          </tr>
-       </tbody>
-</q-markup-table>
-</div>
+            <tr>
+              <th class="text-middle">사진3</th>
+              <th class="text-middle">아이폰3</th>
+              <th class="text-middle">15,300</th>
+              <th class="text-middle"> <span class="removeContainer" style="color: red;" v-on:click="RemoveComment">
+          <i class="fa-solid fa-trash-can" aria-hidden="true"></i> </span> </th>
+            </tr>
+          </tbody>
+        </q-markup-table>
       </div>
-      <hr width="90%">
-      <div style="padding: 10px 40px;">
-        <table style="margin-left:auto;">
-          <td style="font-weight: bold;"> 합계 </td>
-          <td> 18,300</td>
-        </table>
       </div>
 
-    </div>
-</div>
+      <div class="cartTotal">
+        <span
+        style="font-size: 23px;
+        font-family: 'Rubik', sans-serif;
+        margin-left: 20px;">Total</span>
+        <hr width="90%">
 
-  <div class="cart1Button" >
-      <q-btn style="color: white;
-      background-color: #F79a38;
-      width: 180px;
-      height: 30px;
-      display: block;
-      margin-left: 77%;
-      margin-bottom: 50px;" >
-        <div style="font-size: 18px; font-weight: 900;
-        font-family: 'Noto Sans KR', sans-serif;">결제하기</div>
-      </q-btn>
+        <div>
+          <div  style=" margin:0 auto;">
+          <q-markup-table :separator="separator" flat >
+         <tbody>
+            <tr>
+              <th class="text-middle">아이폰</th>
+              <th class="text-middle">1개</th>
+              <th class="text-middle">15,300</th>
+            </tr>
+
+            <tr>
+              <th class="text-middle">아이폰2</th>
+              <th class="text-middle">1개</th>
+              <th class="text-middle">15,300</th>
+            </tr>
+
+            <tr>
+              <th class="text-middle">아이폰3</th>
+              <th class="text-middle">1개</th>
+              <th class="text-middle">15,300</th>
+            </tr>
+         </tbody>
+  </q-markup-table>
   </div>
-<Footer></Footer>
-</template>
+        </div>
+        <hr width="90%">
+        <div style="padding: 10px 40px;">
+          <table style="margin-left:auto;">
+            <td style="font-weight: bold;"> 합계 </td>
+            <td> 18,300</td>
+          </table>
+        </div>
+
+      </div>
+  </div>
+
+    <div class="cart1Button" >
+        <a href="http://localhost:9000/#/cart2">
+        <q-btn style="color: white;
+        background-color: #F79a38;
+        width: 180px;
+        height: 30px;
+        display: block;
+        margin-left: 77%;
+        margin-bottom: 50px;" >
+          <div style="font-size: 18px; font-weight: 900;
+          font-family: 'Noto Sans KR', sans-serif;"> 주문하기</div>
+        </q-btn></a>
+    </div>
+  <Footer></Footer>
+  </template>
 
 
-<script>
-import Header from 'src/components/Home/Header.vue';
-import Footer from '../../components/Home/Footer.vue';
-export default{
-    components: { Header, Footer }
-}
-</script>
+  <script>
+  import Header from 'src/components/Home/Header.vue';
+  import Footer from '../../components/Home/Footer.vue';
+  export default{
+      components: { Header, Footer }
+  }
+  </script>
 
-<style>
-.cartGoods{
+  <style>
+  .cartGoods{
 
-  width: 80%;
-  height: 350px;
-  margin: 60px;
-}
-.cartTitle1{
-   margin: 10px;
-   font-size: 23px;
-   font-family: 'Noto Sans KR', sans-serif;
-   display: flex;
-   gap: 0px 130px;
-   padding: 5px;
+    width: 80%;
+    height: 350px;
+    margin: 60px;
+  }
+  .cartTitle1{
+     margin: 10px;
+     font-size: 23px;
+     font-family: 'Noto Sans KR', sans-serif;
+     display: flex;
+     gap: 0px 130px;
+     padding: 5px;
 
-}
-.cartTotal{
-  width: 30%;
-  margin: 60px;
-  border: 5px solid #FAEBD7;
-  padding: 10px;
-}
+  }
+  .cartTotal{
+    width: 30%;
+    margin: 60px;
+    border: 5px solid #FAEBD7;
+    padding: 10px;
+  }
 
-th{
-  font-family: 'Noto Sans KR', sans-serif;
-  font-weight: bold;
-  font-size: 23px;
-}
+  th{
+    font-family: 'Noto Sans KR', sans-serif;
+    font-weight: bold;
+    font-size: 23px;
+  }
 
-td{
-  font-size: 15px;
-}
+  td{
+    font-size: 15px;
+  }
 
-</style>
+  a{text-decoration: none;}
+  </style>
 
--- client/src/pages/Mypage/MyPage.vue
+++ client/src/pages/Mypage/MyPage.vue
@@ -5,29 +5,29 @@
       <p class="myPageTitle" > My Page</p>
         <div class="myPageSelect" >
           <div class="myPageMember">
-            <a href ="http://localhost:9000/#/userinfo" type="button" style="color: black;">
+            <a href ="http://localhost:9000/#/api/userinfo" type="button" style="color: black;">
             <img src="https://velog.velcdn.com/images/kimjyunny_dev/post/4a9937e2-23f5-4595-bc39-6402a5e42187/image.png" width="80" >
-            <p>회원정보</p>
+            <p>회원정보 수정</p>
           </a>
           </div>
           <div class="myPageOrder">
-            <a href ="http://localhost:9000/#/orderinfo" type="button"  style="color: black;">
+            <a href ="http://localhost:9000/#/api/orderinfo" type="button"  style="color: black;">
             <img src="https://velog.velcdn.com/images/kimjyunny_dev/post/2422b5e0-250f-4a47-b0ec-631ca1c4ee94/image.png" width="80" >
             <p>주문내역</p>
           </a>
           </div>
           <div class="myPageWrite">
-            <a href ="http://localhost:9000/#/writinginfo" type="button"  style="color: black;">
+            <a href ="http://localhost:9000/#/api/writinginfo" type="button"  style="color: black;">
             <img src="https://velog.velcdn.com/images/kimjyunny_dev/post/53ea14e5-f61a-44fb-ba7d-d1c542bc36a7/image.png" width="80" >
             <p>작성한 글</p>
             </a>
           </div>
-          <div class="myPageLike">
+          <!-- <div class="myPageLike">
             <a href ="http://localhost:9000/#/likeinfo" type="button"  style="color: black;">
             <img src="https://velog.velcdn.com/images/kimjyunny_dev/post/bdc5cd07-2946-4985-847c-03689d903d46/image.png" width="80" >
             <p>좋아요</p>
           </a>
-          </div>
+          </div> -->
         </div>
   </div>
   <Footer></Footer>
@@ -62,6 +62,7 @@ export default{
    font-size: 25px;
    font-family: 'Noto Sans KR', sans-serif;
   padding: 30px;
+  padding-left: 170px;
 }
 .myPageMember{
   margin: 10px;

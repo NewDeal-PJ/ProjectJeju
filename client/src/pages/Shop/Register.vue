988e1a00ae3e7c823899982b6c3b62a06f9dd6e4
chaos663 <ssk8571@gmail.com>
2022-09-06 15:33:17 +0900

    임시 푸시
    ㅎ

--- client/src/pages/Shop/Register.vue
+++ client/src/pages/Shop/Register.vue
@@ -9,7 +9,8 @@
             <td style="padding-top: 20px; display: flex;">
               <div class="q-gutter-md form-group"
                 style="width: 400px;">
-                <q-input filled v-model="productNameText" :dense="dense" />
+                <q-input ref="product_nameRef" filled v-model="product_name" :dense="dense" id="product_name" lazy-rules
+                  :rules="product_nameRules" required />
               </div>
             </td>
           </tr>
@@ -18,7 +19,8 @@
             <td style="padding-top: 20px; display: flex;">
               <div class="q-gutter-md form-group"
                 style="width: 400px;">
-                <q-input filled v-model="productPriceText" :dense="dense" />
+                <q-input ref="product_priceRef" filled v-model="product_price" :dense="dense" id="product_price" lazy-rules
+                  :rules="product_priceRules" required />
               </div>
             </td>
           </tr>
@@ -27,13 +29,12 @@
             <td style="padding-top: 20px; display: flex;">
                 <div class="q-pa-md">
                   <div class="q-gutter-md row items-start">
-                    <q-input
-                        @update:model-value="val => { files = val }"
-                        multiple
-                        filled
-                        type="file"
-                        hint="Native file (multiple)"
-                      />
+                    <q-file
+      v-model="image"
+      label="Pick one file"
+      filled
+      style="max-width: 300px"
+    />
                   </div>
               </div>
             </td>
@@ -44,7 +45,7 @@
             <td style="padding-top: 20px; display: flex;">
                 <div class="q-pa-md" style="width: 400px">
                 <q-input
-                  v-model="productDescriptiontext"
+                  v-model="product_content"
                   filled
                   autogrow
                 />
@@ -57,7 +58,7 @@
         <div style="padding: 10px;">
           <div class="registerButton">
             <div class="q-pa-md q-gutter-md form-group">
-              <q-btn type="submit" style="color: white; background-color: #FF9800; width: 300px;
+              <q-btn @click="onsubmit()" type="submit" style="color: white; background-color: #FF9800; width: 300px;
             height: 40px; margin: 0 auto; display: block; ">
               <div style="font-size: 18px; font-weight: 200;
             font-family: 'Noto Sans KR', sans-serif;">등록하기</div>
@@ -69,7 +70,7 @@
           <div style="padding: 10px;">
           <div class="cancelButton">
             <div class="q-pa-md q-gutter-md form-group">
-              <q-btn type="submit" style="color: black; background-color:  #E7E7E7; width: 300px;
+              <q-btn @click="submit_cancel" type="submit" style="color: black; background-color:  #E7E7E7; width: 300px;
             height: 40px; margin: 0 auto; display: block; ">
               <div style="font-size: 18px; font-weight: 200;
             font-family: 'Noto Sans KR', sans-serif;">취소하기</div>
@@ -88,19 +89,96 @@
 import { ref } from 'vue'
 import Header from 'src/components/Home/Header.vue';
 import Footer from '../../components/Home/Footer.vue';
+import { useQuasar } from 'quasar';
+import { reactive } from 'vue';
+import axios from 'axios';
 
 export default{
     setup () {
+      const state = reactive({
+      data: []
+    });
+    const product_name = ref(null)
+    const product_nameRef = ref(null)
+    const product_price = ref(null)
+    const product_priceRef = ref(null)
+    const product_content = ref(null)
+    const image = ref(null)
+    const $q = useQuasar();
+    
     return {
-      text: ref(''),
-      ph: ref(''),
       dense: ref(false),
-      file: ref(null),
-      files: ref(null)
-    }
+      image,
+      state,
+      product_content,
+      product_name,
+      product_nameRef,
+      product_nameRules: [
+        val => (val && val.length > 0) || '상품명을 입력해주세요.',
+        val => (!(/\s/.test(val))) || '상품명은 공백이 들어갈 수 없습니다.',
+      ],
+      product_price,
+      product_priceRef,
+      product_priceRules: [
+        val => (val && val.length > 1) || '10원 이상 입력',
+      ],
+
+
+
+      onsubmit() {
+        // product_name.value.validate()
+        // product_price.value.validate()
+            const args = {
+              product_name : product_name,
+              product_price : product_price,
+              product_image : image,
+              product_content : product_content
+            };
+            if (product_nameRef.value.hasError || product_priceRef.value.hasError) {
+              $q.notify({
+                color: 'negative',
+                message: '필수 정보를 입력해주세요.'
+              })
+            }
+            else {
+              axios
+                .post("/api/shop/register", args)
+                .then((res) => {
+                  console.log(image)
+                  // console.log(image._value[0].name)
+                  // console.log(img._value)
+                  state.data = res.data;
+                  // window.location.href = 'http://localhost:9000/#/api/shop';
+                  $q.notify({
+                    color: 'orange-7',
+                    icon: 'thumb_up',
+                    message: product_name.value + `등록이 완료되었습니다.`,
+                    position: 'center',
+                    timeout: 1200
+                    
+                  })
+                })
+                //redirect logic
+                .catch(function (error) {
+                  // 에러 핸들링
+                  console.log(error);
+                })
+            }
+          },
+          submit_cancel(){
+            window.location.href = 'http://localhost:9000/#/api/shop';
+          },
+          // upload(image) {
+          //   console.log(image)
+          //   this.image = image[0]
+          //   console.log(this.image)
+          
+
+// },
+     }
   },
     components: { Header, Footer }
-  }
+}
 </script>
 
 <style>

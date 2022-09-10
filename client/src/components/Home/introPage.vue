a2dd41e6bcb838be0d198252f1cf0076030912af
김젼이 <wlgus0321@gmail.com>
2022-09-10 17:50:22 +0900

    인트로 페이지

---
+++ client/src/components/Home/introPage.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="introBox"> 인트로 입니다. </div>
+</template>
+
+<script>
+  export default{
+
+  }
+</script>
+
+<style>
+.introBox{
+  background-color: aqua;
+  padding-top: 150px;
+
+}
+
+
+</style>
+

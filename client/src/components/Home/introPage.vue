2ae2c68bd802a4e0aa39a302640d21a009175c07
Soobin Lee <sxxbin418@naver.com>
2022-09-11 12:46:22 +0900

    Intro page 생성
    인트로 슬라이드 이미지를 생성하였습니다
    (이미지 2개 새로 추가할 예정, 버튼 위치 조정)

--- client/src/components/Home/introPage.vue
+++ client/src/components/Home/introPage.vue
@@ -1,20 +1,81 @@
+<!-- 
+v-model:fullscreen="fullscreen"
+vertical
+swipeable
+control-color="white"
+navigation-icon="radio_button_unchecked"
+padding
+height="695px"
+class="bg-purple text-white shadow-1 rounded-borders" -->
+
+
 <template>
-  <div class="introBox"> 인트로 입니다. </div>
+  <div class="q-pa-md">
+    <q-carousel
+      animated
+      v-model="slide"
+      navigation
+      infinite
+      :autoplay="autoplay"
+      arrows
+      transition-prev="slide-right"
+      transition-next="slide-left"
+      height="713px"
+      @mouseenter="autoplay = false"
+      @mouseleave="autoplay = true"
+    >
+    <q-carousel-slide :name="1" img-src="https://velog.velcdn.com/images/kimjyunny_dev/post/072d9c43-7183-4152-8048-27a6e21df906/image.jpeg"  />
+    
+    <q-carousel-slide :name="2" img-src="https://velog.velcdn.com/images/kimjyunny_dev/post/072d9c43-7183-4152-8048-27a6e21df906/image.jpeg" />
+    <q-carousel-slide :name="3" img-src="https://velog.velcdn.com/images/kimjyunny_dev/post/072d9c43-7183-4152-8048-27a6e21df906/image.jpeg" />
+    </q-carousel>
+  </div>
+  <div class="q-pa-md q-gutter-sm">
+    <q-btn class="glossy" rounded color="orange" label="START"></q-btn>
+  </div>
+
+
+
+  
 </template>
 
 <script>
-  export default{
+import { ref } from 'vue'
 
+export default {
+  setup () {
+    return {
+      slide: ref(1),
+      autoplay: ref(true)
+    }
   }
-</script>
-
-<style>
-.introBox{
-  background-color: aqua;
-  padding-top: 150px;
-
 }
 
 
+</script>
+
+<style>
+.q-pa-md{
+  padding: 0px 0px;
+}
+
+
+.glossy{
+  margin: auto;
+  display: block;
+
+
+}
+
 </style>
+
+
+
+
+
+
+
+
+
+
 
--- client/src/pages/IndexPage.vue
+++ client/src/pages/IndexPage.vue
@@ -2,21 +2,21 @@
 q-page
   Header
   IntroPage
-  HomeMap
-  MapTracking
-  BestHotel
-  BestRestaurant
-  Footer
+  //- HomeMap
+  //- MapTracking
+  //- BestHotel
+  //- BestRestaurant
+  //- Footer
 </template>
 
 <script>
 import Header from '../components/Home/Header.vue';
 
-import HomeMap from 'src/components/Home/HomeMap.vue';
-import Footer from '../components/Home/Footer.vue';
-import BestHotel from 'src/components/Home/BestHotel.vue';
-import BestRestaurant from '../components/Home/BestRestaurant.vue';
-import MapTracking from '../components/Home/mapTracking.vue';
+// import HomeMap from 'src/components/Home/HomeMap.vue';
+// import Footer from '../components/Home/Footer.vue';
+// import BestHotel from 'src/components/Home/BestHotel.vue';
+// import BestRestaurant from '../components/Home/BestRestaurant.vue';
+// import MapTracking from '../components/Home/mapTracking.vue';
 import IntroPage from 'src/components/Home/introPage.vue';
 
 
@@ -29,11 +29,11 @@ import IntroPage from 'src/components/Home/introPage.vue';
 export default {
   components: {
     Header,
-    HomeMap,
-    Footer,
-    BestHotel,
-    BestRestaurant,
-    MapTracking,
+    // HomeMap,
+    // Footer,
+    // BestHotel,
+    // BestRestaurant,
+    // MapTracking,
     IntroPage
 }
 }

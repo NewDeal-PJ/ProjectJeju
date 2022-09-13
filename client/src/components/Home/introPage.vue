a1c8014c4aa976f0a452d355220bb6e56ff3ad69
김젼이 <wlgus0321@gmail.com>
2022-09-13 23:27:54 +0900

    Update introPage.vue

--- client/src/components/Home/introPage.vue
+++ client/src/components/Home/introPage.vue
@@ -16,18 +16,18 @@
         <q-carousel-slide :name="1" img-src="https://velog.velcdn.com/images/kimjyunny_dev/post/072d9c43-7183-4152-8048-27a6e21df906/image.jpeg" />
         <q-carousel-slide :name="2" img-src="https://blog.kakaocdn.net/dn/o1KIw/btqu9mflPY6/rGk1mM3iugV1c6jj9Z3E80/img.jpg" />
         <q-carousel-slide :name="3" img-src="https://wehotel.co.kr/wp-content/uploads/sites/35/2019/06/Spring-of-Halla-mountain_1920x1080.jpg" />
-        
+
         <template v-slot:control>
           <q-carousel-control
             position="bottom"
             :offset="[16, 8]"
             class="text-white text-center rounded-borders"
             style="background: rgba(255, 255, 255, .2); padding: 20px 8px; margin-bottom: 50px">
-  
+
           <div>
             <span> 어서오세요 제주 올레 입니다.<br> 어서오세요 제주 올레 입니다.</span>
-           
-            
+
+
           </div>
             <div>
               <a href="http://localhost:9000/#/api/map">
@@ -36,18 +36,18 @@
                 </div>
               </a>
             </div>
-          
-  
+
+
           </q-carousel-control>
         </template>
-  
+
       </q-carousel>
     </div>
   </template>
-  
+
   <script>
   import { ref } from 'vue'
-  
+
   export default {
     setup () {
       return {
@@ -57,29 +57,25 @@
     }
   }
   </script>
-  
+
   <style>
-  
+
     .glossy{
       margin: auto;
       display: block;
     }
-    
+
     a{text-decoration: none;}
-    
-    
-    body{
-      margin: 70px 0px 0px 0px;
-    }
-    
-    </style> 
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
+
+
+
+    </style>
+
+
+
+
+
+
+
+
+

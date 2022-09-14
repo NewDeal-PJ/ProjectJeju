317704624b0446755d18937beb23e43416cf1e36
Soobin Lee <sxxbin418@naver.com>
2022-09-14 13:46:06 +0900

    Update FoodDetail.vue
    Food image 자동 슬라이드 설정

--- client/src/pages/FoodDetail.vue
+++ client/src/pages/FoodDetail.vue
@@ -3,25 +3,23 @@
   <body>
     <div class="foodDetailPic">
       <div class="q-pa-sm" style="width: 60%; padding: 20px;">
-        <q-carousel animated v-model="slide" navigation infinite :autoplay="autoplay" arrows transition-prev="slide-right"
-          transition-next="slide-left" @mouseenter="autoplay = false" @mouseleave="autoplay = true">
-
-          <!-- <q-carousel-slide :name="1" img-src={{ homeimg }} />
-            <q-carousel-slide :name="2" img-src="https://cdn.quasar.dev/img/parallax1.jpg" />
-            <q-carousel-slide :name="3" img-src="https://cdn.quasar.dev/img/parallax2.jpg" />
-            <q-carousel-slide :name="4" img-src="https://cdn.quasar.dev/img/quasar.jpg" /> -->
-
-          <!-- <q-carousel-slide
-                v-for = "slide in imgjsdata"
-                :name = "slide.path"
-                :img-src = "slide.imgurl">
-              </q-carousel-slide> -->
-
+        <q-carousel 
+        animated 
+        v-model="slide" 
+        navigation 
+        infinite 
+        :autoplay="autoplay" 
+        arrows 
+        transition-prev="slide-right"
+        transition-next="slide-left" 
+        @mouseenter="autoplay = false" 
+        @mouseleave="autoplay = true"
+        >
           <q-carousel-slide v-for="slide in imgjsdata" :name="slide.id" :img-src="slide.imgurl">
           </q-carousel-slide>
-
         </q-carousel>
       </div>
+
       <!-- <div class="foodDetailInfo"> -->
       <div class="foodDetailInfo" v-for="dataItem in jsdata" :key="dataItem.STOREID" :name="dataItem.STOREID">
         <!-- <p>🍽️식당이름</p> -->
@@ -93,6 +91,7 @@ export default {
     })
     return {
       slide: ref(1),
+      autoplay: ref(true),
       fullscreen: ref(false),
       quality: ref(3),
       submitResult,

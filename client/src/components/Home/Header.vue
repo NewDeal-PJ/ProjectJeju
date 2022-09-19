8d127460d1131d80110da42252922b545dc658a7
김젼이 <wlgus0321@gmail.com>
2022-09-19 21:38:59 +0900

    수정 완

--- client/src/components/Home/Header.vue
+++ client/src/components/Home/Header.vue
@@ -1,5 +1,10 @@
 <template>
-  <header :class="{original_color: scrollPosition < 80, change_color: scrollPosition > 80}">
+  <header
+    :class="{
+      original_color: scrollPosition < 80,
+      change_color: scrollPosition > 80,
+    }"
+  >
     <nav>
       <div class="branding">
         <i class="fas fa-shoe-prints"></i>
@@ -8,15 +13,17 @@
       <ul v-show="!mobile" class="navigation">
         <li><a href="#/api/map">Home</a></li>
         <li><a href="#/api/shop">Shop</a></li>
-        <li><a href="#/api/cart1">Cart({{cart.length}})</a></li>
+        <li>
+          <a href="#/api/cart1">Cart({{ cart.length }})</a>
+        </li>
         <a v-if="account.id">
-          <li><a :href="myPageUrl + account.id ">MyPage</a></li>
+          <li><a :href="myPageUrl + account.id">MyPage</a></li>
         </a>
         <a v-if="social.id">
-          <li><a :href="myPageUrl + social.id ">MyPage</a></li>
+          <li><a :href="myPageUrl + social.id">MyPage</a></li>
         </a>
         <div v-if="account.id || social.id">
-          <li><a href="#/api/logout" @click='logout()'>LogOut</a></li>
+          <li><a href="#/api/logout" @click="logout()">LogOut</a></li>
         </div>
 
         <div v-else>
@@ -25,22 +32,29 @@
         <li><a href="#/api/CustomerService">Q&A</a></li>
       </ul>
       <div class="icon">
-        <i @click="toggleMobileNav" v-show="mobile" class="fas fa-bars" :class="{ 'icon-active': mobileNav }"></i>
+        <i
+          @click="toggleMobileNav"
+          v-show="mobile"
+          class="fas fa-bars"
+          :class="{ 'icon-active': mobileNav }"
+        ></i>
       </div>
       <transition name="mobile-nav">
         <ul v-show="mobileNav" class="dropdown-nav">
           <li><a href="#/api/map">Home</a></li>
           <li><a href="#/api/shop">Shop</a></li>
-          <li><a href="#/api/cart1">Cart({{cart.length}})</a></li>
+          <li>
+            <a href="#/api/cart1">Cart({{ cart.length }})</a>
+          </li>
 
           <a v-if="account.id">
             <li><a :href="myPageUrl + account.id">MyPage</a></li>
           </a>
           <a v-if="social.id">
-          <li><a :href="myPageUrl + social.id ">MyPage</a></li>
-        </a>
+            <li><a :href="myPageUrl + social.id">MyPage</a></li>
+          </a>
           <div v-if="account.id || social.id">
-            <li><a href="#/api/logout" @click='logout()'>LogOut</a></li>
+            <li><a href="#/api/logout" @click="logout()">LogOut</a></li>
           </div>
 
           <div v-else>
@@ -55,29 +69,29 @@
 
 <script>
 import axios from "axios";
-import { reactive } from 'vue';
-import { ref } from 'vue';
-import { useQuasar } from 'quasar';
+import { reactive } from "vue";
+import { ref } from "vue";
+import { useQuasar } from "quasar";
 
 export default {
   name: "navigation",
   data() {
     const account = {
-      id: '',
-      name: ''
-    }
-    const  social = {
-        id : '',
-        method : '',
-        nickname : ''
-    }
+      id: "",
+      name: "",
+    };
+    const social = {
+      id: "",
+      method: "",
+      nickname: "",
+    };
     // 로그인 정보 담아서 보내주려면 객체를 만들어줘야죠~
     const form = {
       loginId: "",
-      loginPw: ""
-    }
-    const $q = useQuasar()
-    const cart = ref([])
+      loginPw: "",
+    };
+    const $q = useQuasar();
+    const cart = ref([]);
     const myPageUrl = "#/api/mypage/";
     return {
       scrolledNav: null,
@@ -91,23 +105,24 @@ export default {
       account,
       social,
       form,
-      cart: JSON.parse(localStorage.getItem("cart")) || ""
+      cart: JSON.parse(localStorage.getItem("cart")) || "",
     };
   },
   created() {
-    window.addEventListener('resize', this.checkScreen);
+    window.addEventListener("resize", this.checkScreen);
     this.checkScreen();
     // 백엔드의 계정정보를 호출
     axios.get("http://localhost:3000/api/login").then((res) => {
       this.account = res.data;
-      if(this.account.id === ''){
-        window.location.href = '#/api/login';
-        alert("로그인 해주세요")
+      if (this.account.id === "") {
+        window.location.href = "#/api/login";
+        alert("로그인 해주세요");
       }
     });
     // 백엔드의 소셜 계정정보를 호출
     axios.get("http://localhost:3000/api/kakao_login").then((res) => {
       this.social = res.data;
+      console.log(this.social);
     });
   },
   mounted() {
@@ -127,9 +142,9 @@ export default {
       this.scrolledNav = false;
     },
 
-    updateScroll(){
-         this.scrollPosition = window.scrollY
-      },
+    updateScroll() {
+      this.scrollPosition = window.scrollY;
+    },
 
     checkScreen() {
       this.windowWidth = window.innerWidth;
@@ -144,16 +159,16 @@ export default {
     logout() {
       axios.delete("http://localhost:3000/api/logout").then((res) => {
         this.$q.notify({
-          color: 'teal',
-          position: 'center',
-          message: '로그아웃 되었습니다.'
-        })
+          color: "teal",
+          position: "center",
+          message: "로그아웃 되었습니다.",
+        });
         this.account.name = "";
         this.account.id = "";
         this.social.id = "";
         this.social.method = "";
         this.social.nickname = "";
-        window.location.href = '#/api/login';
+        window.location.href = "#/api/login";
       });
     },
   },
@@ -162,13 +177,11 @@ export default {
 
 <style lang="scss" scoped>
 header {
-
   z-index: 99;
   width: 100%;
   position: sticky;
   transition: 0.5s ease all;
   top: 0;
-
 
   nav {
     position: relative;
@@ -178,9 +191,8 @@ header {
     width: 90%;
     margin: 0 auto;
 
-
     .branding > a {
-      font-family: 'Bungee', cursive;
+      font-family: "Bungee", cursive;
       font-size: 35px;
       white-space: nowrap;
     }
@@ -223,14 +235,12 @@ header {
     .branding {
       display: flex;
       align-items: center;
-      font-size: 29px;
+      font-size: 2rem;
 
       img {
         width: 50px;
         transition: 0.5s ease all;
       }
-
-
     }
 
     .navigation {
@@ -238,12 +248,9 @@ header {
       align-items: center;
       flex: 1;
       justify-content: flex-end;
-      font-family: 'Raleway', sans-serif;
+      font-family: "Raleway", sans-serif;
       font-size: 16px;
-
     }
-
-
 
     .icon {
       display: flex;
@@ -252,8 +259,6 @@ header {
       top: 0;
       right: 24px;
       height: 100%;
-
-
 
       i {
         cursor: pointer;
@@ -282,8 +287,8 @@ header {
       padding-left: 0;
 
       li > a {
-          color: orange;
-        }
+        color: orange;
+      }
 
       li {
         margin-left: 0;
@@ -329,8 +334,6 @@ a {
   text-decoration: none;
 }
 
-
-
 .original_color {
   background-color: antiquewhite;
   color: green;
@@ -344,25 +347,23 @@ a {
     color: orange;
   }
 
-  a{
+  a {
     color: orange;
   }
-
 }
 
-.change_color{
+.change_color {
   background-color: white;
-  	// #ECF7E1;
+  // #ECF7E1;
 
   color: black;
   width: 100%;
 
-
-  nav > .icon{
+  nav > .icon {
     color: green;
   }
 
-  nav > .branding > i{
+  nav > .branding > i {
     color: green;
   }
 
@@ -370,12 +371,8 @@ a {
     color: orange;
   }
 
-  a{
+  a {
     color: black;
   }
-
-
-  }
-
-
+}
 </style>
--- client/src/pages/Mypage/MyPage.vue
+++ client/src/pages/Mypage/MyPage.vue
@@ -1,104 +1,104 @@
 <template>
   <Header></Header>
   <body>
-    <div>
-      <div class="myPageBox">
-        <p class="myPageTitle"> My Page</p>
-        <div class="myPageSelect" >
-          <div class="myPageMember" v-if="account.id">
-            <a href="#/api/userinfo" type="button" style="color: black;">
-              <img src="https://velog.velcdn.com/images/kimjyunny_dev/post/4a9937e2-23f5-4595-bc39-6402a5e42187/image.png"
-                >
-              <p>회원정보 수정</p>
-            </a>
-          </div>
-          <div class="myPageMember" v-if="social.id">
-            <a @click="social_info()" type="button" style="color: black;">
-              <img src="https://velog.velcdn.com/images/kimjyunny_dev/post/4a9937e2-23f5-4595-bc39-6402a5e42187/image.png"
-                >
-              <p>회원정보 수정</p>
-            </a>
-          </div>
-          <div class="myPageOrder">
-            <a href="#/api/orderinfo" type="button" style="color: black;">
-              <img src="https://velog.velcdn.com/images/kimjyunny_dev/post/2422b5e0-250f-4a47-b0ec-631ca1c4ee94/image.png"
-         >
-              <p>주문내역</p>
-            </a>
-          </div>
-          <div class="myPageWrite">
-            <a :href="writinginfoUrl" type="button" style="color: black;">
-              <img src="https://velog.velcdn.com/images/kimjyunny_dev/post/53ea14e5-f61a-44fb-ba7d-d1c542bc36a7/image.png"
-           >
-              <p>작성한 글</p>
-            </a>
-          </div>
-          <div class="myMapTracking">
-              <a href ="#/api/mymaptracking" type="button"  style="color: black;">
-              <img src="https://velog.velcdn.com/images/kimjyunny_dev/post/36ffecab-fbd6-4cba-b352-106310a3d3c7/image.png"  >
-              <p>나의 <br/>트래킹</p>
-            </a>
+    <p class="myPageTitle">My Page</p>
+    <div class="myPageSelect">
+      <section class="content">
+        <div class="flexbox">
+          <div class="item">
+            <div class="myPageMember" v-if="account.id">
+              <a href="#/api/userinfo" type="button" style="color: black;">
+                <img
+                  src="https://velog.velcdn.com/images/kimjyunny_dev/post/4a9937e2-23f5-4595-bc39-6402a5e42187/image.png"
+                />
+                <p>회원정보 <br />수정</p>
+              </a>
             </div>
+          </div>
+
+          <div class="item">
+            <div class="myPageOrder">
+              <a href="#/api/orderinfo" type="button" style="color: black;">
+                <img
+                  src="https://velog.velcdn.com/images/kimjyunny_dev/post/2422b5e0-250f-4a47-b0ec-631ca1c4ee94/image.png"
+                />
+                <p>주문<br />내역</p>
+              </a>
+            </div>
+          </div>
+
+          <div class="item">
+            <div class="myPageWrite">
+              <a :href="writinginfoUrl" type="button" style="color: black;">
+                <img
+                  src="https://velog.velcdn.com/images/kimjyunny_dev/post/53ea14e5-f61a-44fb-ba7d-d1c542bc36a7/image.png"
+                />
+                <p>
+                  작성한<br />
+                  글
+                </p>
+              </a>
+            </div>
+          </div>
         </div>
-      </div>
-      <Footer></Footer>
+      </section>
     </div>
+
+    <Footer></Footer>
   </body>
 </template>
 
-
 <script>
-import axios from 'axios';
-import Header from 'src/components/Home/Header.vue';
-import Footer from '../../components/Home/Footer.vue';
-import { useQuasar } from 'quasar';
+import axios from "axios";
+import Header from "src/components/Home/Header.vue";
+import Footer from "../../components/Home/Footer.vue";
+import { useQuasar } from "quasar";
 import { useRoute } from "vue-router";
 import { onMounted } from "vue";
 export default {
   data() {
     const account = {
-      id: '',
-      name: ''
-    }
-    const  social = {
-        id : '',
-        method : '',
-        nickname : ''
-    }
-    return{
+      id: "",
+      name: "",
+    };
+    const social = {
+      id: "",
+      method: "",
+      nickname: "",
+    };
+    return {
       account,
-      social
-    }
+      social,
+    };
   },
   setup() {
-    const route = useRoute()
+    const route = useRoute();
     const $q = useQuasar();
     onMounted(() =>
       axios.get("http://localhost:3000/api/login").then((res) => {
-        const id = res.data.id
+        const id = res.data.id;
         if (id !== route.params.id) {
           $q.notify({
-            color: 'negative',
-            position: 'center',
-            message: '잘못된 접근입니다.'
-          })
-          window.location.href = '#/';
+            color: "negative",
+            position: "center",
+            message: "잘못된 접근입니다.",
+          });
+          window.location.href = "#/";
         }
-      }));
-      const writinginfoUrl = "#/api/writinginfo?auth="+route.params.id;
-      return{
-        writinginfoUrl,
-        $q
-      }
+      })
+    );
+    const writinginfoUrl = "#/api/writinginfo?auth=" + route.params.id;
+    return {
+      writinginfoUrl,
+    };
   },
-  mounted (){
-// 백엔드의 계정정보를 호출
-axios.get("http://localhost:3000/api/login").then((res) => {
+  mounted() {
+    // 백엔드의 계정정보를 호출
+    axios.get("http://localhost:3000/api/login").then((res) => {
       this.account = res.data;
-      console.log(this.account)
-      if(this.account.id === ''){
-        window.location.href = '#/api/login';
-        alert("로그인 해주세요")
+      if (this.account.id === "") {
+        window.location.href = "#/api/login";
+        alert("로그인 해주세요");
       }
     });
     // 백엔드의 소셜 계정정보를 호출
@@ -106,33 +106,21 @@ axios.get("http://localhost:3000/api/login").then((res) => {
       this.social = res.data;
     });
   },
-  methods : {
-    social_info(){
-      this.$q.notify({
-            color: 'negative',
-            position: 'center',
-            message: '잘못된 접근입니다.'
-          })
-    }
 
-  },
-
-  components: { Header, Footer }
-}
-
+  components: { Header, Footer },
+};
 </script>
 
 <style>
 .myPageTitle {
   font-size: 35px;
   font-weight: bold;
-  font-family: 'Ubuntu', sans-serif;
+  font-family: "Ubuntu", sans-serif;
   text-align: center;
   margin-top: 70px;
-
 }
 
-.myPageSelect {
+/* .myPageSelect {
   margin: 0 auto;
   width: 70%;
   display: flex;
@@ -140,68 +128,90 @@ axios.get("http://localhost:3000/api/login").then((res) => {
   margin-bottom: 20px;
   text-align: center;
   font-size: 25px;
-  font-family: 'Noto Sans KR', sans-serif;
+  font-family: "Noto Sans KR", sans-serif;
   padding: 30px;
-
-}
+} */
 
 .myPageMember {
-  margin: 10px;
-  width: 25%;
-  border: 4px solid #FAEBD7;
+  border: 4px solid #faebd7;
   border-radius: 5px;
-  padding: 5%;
+  margin: 10px;
 
+  padding: 20% 0%;
+}
+
+.myPageMember > a > img {
+  max-width: 50%;
 }
 
 .myPageOrder {
-  border: 4px solid #FAEBD7;
+  border: 4px solid #faebd7;
   border-radius: 5px;
   margin: 10px;
-  width: 25%;
-  padding: 5%;
+
+  padding: 20% 0%;
+}
+
+.myPageOrder > a > img {
+  max-width: 50%;
 }
 
 .myPageWrite {
-  border: 4px solid #FAEBD7;
+  border: 4px solid #faebd7;
   border-radius: 5px;
   margin: 10px;
-  width: 25%;
-  padding: 5%;
 
+  padding: 20% 0%;
 }
 
-.myMapTracking {
-  border: 4px solid #FAEBD7;
-  border-radius: 5px;
-  margin: 10px;
-  width: 25%;
-  padding: 5%;
+.myPageWrite > a > img {
+  max-width: 50%;
 }
 
 a {
-  text-decoration: none
+  text-decoration: none;
 }
 
-.myPageMember:hover{
+.myPageMember:hover {
   background-color: bisque;
 }
 
-.myPageOrder:hover{
+.myPageOrder:hover {
   background-color: bisque;
 }
 
-.myPageWrite:hover{
+.myPageWrite:hover {
   background-color: bisque;
 }
 
-.myMapTracking:hover{
-  background-color: bisque;
+.content {
+  max-width: 800px;
+  margin: 0 auto;
 }
 
-img{
-  width: 80;
-  max-width: 100%;
+.flexbox {
+  display: flex;
+  flex-wrap: wrap;
+  gap: 1em;
+  flex-grow: 0;
 }
 
+.item {
+  min-height: 200px;
+  flex-basis: 150px;
+  flex-grow: 1;
+  width: 70%;
+  margin-top: 20px;
+  margin-bottom: 20px;
+
+  /* margin: 0 auto;
+  width: 70%;
+  display: flex;
+  margin-top: 20px;
+  margin-bottom: 20px;
+  padding: 30px; */
+  text-align: center;
+  font-size: 25px;
+  font-family: "Noto Sans KR", sans-serif;
+}
 </style>
